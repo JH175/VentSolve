015ccd054850269e47ae4076bffb43455b7e6da9
Jake H <jake@hink.dev>
2023-12-20 08:41:11 +0300

    debug next-auth, debug posts api route by moving api folder to posts folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,30 @@
 import PostPreview from '@/components/PostPreview';
 import Link from 'next/link';
+import prisma from '@/lib/prisma';
 
-export default function Home() {
+export default async function Home() {
+  const posts = await prisma.post.findMany();
   return (
     <div className='flex flex-col justify-center items-center gap-10'>
       <h2 className='text-2xl'>Join the global fight against pain points.</h2>
-      <div className='flex flex-col gap-2 justify-center items-center'>
-        <PostPreview
-          title='Filling out an application after submitting a resume with the same information.'
-          score={100}
-          votes={92}
-          tags={['hr', 'ux', 'hiring']}
-          bounty={true}
-        />
-        <PostPreview
-          title='Manual workflow, multiple forms, and no reliable tracking for international container shipments.'
-          score={80}
-          votes={20}
-          tags={['shipping', 'business', 'freight']}
-          bounty={false}
-        />
-        <PostPreview
-          title='Tedious process and duplication of data in Excel every time information is required from multiple people.'
-          score={60}
-          votes={130}
-          tags={['excel', 'software', 'data']}
-          bounty={true}
-        />
-        <PostPreview
-          title='Inefficient process to update and distribute company policies and documents.'
-          score={40}
-          votes={18}
-          tags={['software', 'business']}
-          bounty={false}
-        />
-        <PostPreview
-          title='Lack of interoperability between PCR/EHR systems.'
-          score={20}
-          votes={13}
-          tags={['software', 'healthcare']}
-          bounty={false}
-        />
-        <Link
-          href='/posts'
-          className='text-orange-500 hover:text-orange-600 text-lg'
-        >
-          See more posts...
-        </Link>
+      <div className='p-10 flex flex-col gap-2 justify-center items-center'>
+        {posts?.map((post: any) => (
+          <PostPreview
+            key={post.id}
+            title={post.title}
+            score={post.score}
+            votes={post.votes}
+            tags={post.tags}
+            bounty={false}
+          />
+        ))}
       </div>
+      <Link
+        href='/posts'
+        className='text-orange-500 hover:text-orange-600 text-lg'
+      >
+        See more posts...
+      </Link>
     </div>
   );
 }
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,79 +1,29 @@
 import PostPreview from '@/components/PostPreview';
-import Link from 'next/link';
+import { Post } from '@prisma/client';
 
-export default function Posts() {
+const getPosts = async () => {
+  const res = await fetch('http://localhost:3000/posts/api', {
+    method: 'GET',
+  });
+  const data = await res.json();
+  return data;
+};
+
+export default async function Posts() {
+  const { posts } = await getPosts();
   return (
     <div className='p-10 flex flex-col gap-2 justify-center items-center'>
-      <PostPreview
-        title='Filling out an application after submitting a resume with the same information.'
-        score={100}
-        votes={92}
-        tags={['hr', 'ux', 'hiring']}
-        bounty={true}
-      />
-      <PostPreview
-        title='Manual workflow, multiple forms, and no reliable tracking for international container shipments.'
-        score={80}
-        votes={20}
-        tags={['shipping', 'business', 'freight']}
-        bounty={false}
-      />
-      <PostPreview
-        title='Tedious process and duplication of data in Excel every time information is required from multiple people.'
-        score={60}
-        votes={130}
-        tags={['excel', 'software', 'data']}
-        bounty={true}
-      />
-      <PostPreview
-        title='Inefficient process to update and distribute company policies and documents.'
-        score={40}
-        votes={18}
-        tags={['software', 'business']}
-        bounty={false}
-      />
-      <PostPreview
-        title='Lack of interoperability between PCR/EHR systems.'
-        score={20}
-        votes={13}
-        tags={['software', 'healthcare']}
-        bounty={false}
-      />
-      <PostPreview
-        title='Filling out an application after submitting a resume with the same information.'
-        score={100}
-        votes={92}
-        tags={['hr', 'ux', 'hiring']}
-        bounty={true}
-      />
-      <PostPreview
-        title='Manual workflow, multiple forms, and no reliable tracking for international container shipments.'
-        score={80}
-        votes={20}
-        tags={['shipping', 'business', 'freight']}
-        bounty={false}
-      />
-      <PostPreview
-        title='Tedious process and duplication of data in Excel every time information is required from multiple people.'
-        score={60}
-        votes={130}
-        tags={['excel', 'software', 'data']}
-        bounty={true}
-      />
-      <PostPreview
-        title='Inefficient process to update and distribute company policies and documents.'
-        score={40}
-        votes={18}
-        tags={['software', 'business']}
-        bounty={false}
-      />
-      <PostPreview
-        title='Lack of interoperability between PCR/EHR systems.'
-        score={20}
-        votes={13}
-        tags={['software', 'healthcare']}
-        bounty={false}
-      />
+      {Array.isArray(posts) &&
+        posts.map((post: Post) => (
+          <PostPreview
+            key={post.id}
+            title={post.title}
+            score={post.score}
+            votes={post.votes}
+            bounty={false}
+            tags={[]}
+          />
+        ))}
     </div>
   );
 }
